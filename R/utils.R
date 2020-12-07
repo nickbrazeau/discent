b3800583a3890edd9e98122a1e590103821fde4b
nickbrazeau <nbrazeau@med.unc.edu>
2020-12-07 16:17:43 -0500

    add progress bar

--- R/utils.R
+++ R/utils.R
@@ -10,3 +10,16 @@
 #' @usage lhs \%>\% rhs
 NULL
 
+#------------------------------------------------
+# update progress bar.
+# pb_list = list of progress bar objects
+# name = name of this progress bar
+# i = new value of bar
+# max_i = max value of bar (close when reach this value)
+#' @noRd
+update_progress <- function(pb_list, name, i, max_i) {
+  setTxtProgressBar(pb_list[[name]], i)
+  if (i == max_i) {
+    close(pb_list[[name]])
+  }
+}
