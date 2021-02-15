c6f2a3b1f71ce68bd0378655552eff0e918a1d30
nickbrazeau <nbrazeau@med.unc.edu>
2021-02-15 10:54:06 -0500

    better test

--- tests/testthat/test-runmod.R
+++ tests/testthat/test-runmod.R
@@ -3,19 +3,32 @@ test_that("model runs works", {
   # simulation some data
   #......................
   set.seed(48)
-  K_gendist_geodist <- tibble::tibble(
-    smpl1 = paste0("smpl", 1:26),
-    smpl2 = paste0("smpl", 27:52),
-    locat1 = sample(LETTERS, size = 26, replace = T),
-    locat2 = sample(LETTERS, size = 26, replace = T),
-    gendist = rbeta(26, 0.5, 0.5),
+  locats <- tibble::tibble(
+    smpl = paste0("smpl", 1:10),
+    locat = sample(LETTERS, size = 10, replace = F)
   )
+  # bring together
+  K_gendist_geodist <- tibble::as_tibble(t(combn(locats$smpl, 2)))
+  colnames(K_gendist_geodist)[1] <- "smpl1"
+  colnames(locats)[1] <- "smpl1"
+  K_gendist_geodist <- dplyr::left_join(K_gendist_geodist, locats, by = "smpl1")
+  colnames(K_gendist_geodist)[2] <- "smpl2"
+  colnames(locats)[1] <- "smpl2"
+  K_gendist_geodist <- dplyr::left_join(K_gendist_geodist, locats, by = "smpl2")
+  colnames(K_gendist_geodist) <- c("smpl1", "smpl2", "locat1", "locat2")
+
+  # now remove selfs (not necessary since we sample w/out replacement above, but for transparency)
+  K_gendist_geodist <- K_gendist_geodist %>%
+    dplyr::filter(locat1 != locat2)
+
+  # make up some genetic distance
+  K_gendist_geodist$gendist <- rbeta(nrow(K_gendist_geodist), 0.5, 0.5)
+
   geodist <- K_gendist_geodist %>%
     dplyr::select(c("locat1", "locat2")) %>%
     dplyr::filter(!duplicated(.))
   geodist <- geodist %>%
-    dplyr::mutate(geodist = rlnorm(dplyr::n(), 3, 1),
-                  geodist = ifelse(locat1 == locat2, 0, geodist))
+    dplyr::mutate(geodist = rlnorm(dplyr::n(), 3, 1))
   # locat names
   locatnames <- unique(c(geodist$locat1, geodist$locat2))
   # bring together
