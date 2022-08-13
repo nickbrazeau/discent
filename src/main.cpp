20e11278032190179a98702a405a45346bbbca0b
nickbrazeau <nbrazeau1@gmail.com>
2022-08-13 13:49:46 -0400

    gradient test by hand

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,17 @@ using namespace std;
 Rcpp::List deme_inbreeding_coef_cpp(Rcpp::List args, Rcpp::List args_functions, Rcpp::List args_progress) {
   // overflow cost parameter
   const double OVERFLO_DOUBLE = DBL_MAX/1000000.0;
-  // extract proposed Fis for each K
-  vector<double> fvec = rcpp_to_vector_double(args["fvec"]); // proposed Inb. Coeff. for demes
-  // extract proposed M and boundaries
-  double m = rcpp_to_double(args["m"]); // proposed global M of migration
+  // extract proposed proposed Inb. Coeff. (Fis) for each K (deme)
+  vector<double> fvec = rcpp_to_vector_double(args["fvec"]);
+  // extract proposed global M of migration
+  double m = rcpp_to_double(args["m"]);
   // get dims
   int n_Demes = fvec.size();
   int n_Kpairmax =  rcpp_to_int(args["n_Kpairmax"]);
 
-  // observed genetic data
-  vector<double> gendist = rcpp_to_vector_double(args["gendist"]); // pairwise sample genetic distances
-  // recast array
+  // observed pairwise sample genetic distances
+  vector<double> gendist = rcpp_to_vector_double(args["gendist"]);
+  // recast to array from vector (perserving structure)
   vector<vector<vector<double>>> gendist_arr(n_Demes, vector<vector<double>>(n_Demes, vector<double>(n_Kpairmax)));
   int geniter = 0;
   for (int i = 0; i < n_Kpairmax; i++) {
@@ -30,6 +30,7 @@ Rcpp::List deme_inbreeding_coef_cpp(Rcpp::List args, Rcpp::List args_functions, 
       }
     }
   }
+
   // observed geo data
   vector<double> geodist = rcpp_to_vector_double(args["geodist"]); // pairwise sample geo distances
   vector<vector<double>> geodist_mat(n_Demes, vector<double>(n_Demes));
@@ -57,17 +58,12 @@ Rcpp::List deme_inbreeding_coef_cpp(Rcpp::List args, Rcpp::List args_functions, 
   // items for momentum
   vector<vector<double>> fi_update(steps, vector<double>(n_Demes));
   vector<double> m_update(steps);
-  m_update[0] = 0.0;
 
   // items of interest to keep track of
   vector<double> cost(steps);
   vector<double> m_run(steps);
-  m_run[0] = m;
   vector<vector<double>> fi_run(steps, vector<double>(n_Demes));
-  for (int i = 0; i < n_Demes; i++) {
-    fi_run[0][i] = fvec[i];
-    fi_update[0][i] = 0.0;
-  }
+
   // to delete
   vector<double> f_learners(steps);
   f_learners[0] = f_learningrate;
@@ -77,6 +73,35 @@ Rcpp::List deme_inbreeding_coef_cpp(Rcpp::List args, Rcpp::List args_functions, 
   f_grad2[0] = 0;
   vector<double> m_grad2(steps);
   m_grad2[0] = 0;
+  double mcall = 0;
+
+
+  //-------------------------------
+  // initialize storage vectors
+  // calc initial cost for user proposed Fs and M
+  //-------------------------------
+  for (int i = 0; i < n_Demes; i++) {
+    fi_run[0][i] = fvec[i];
+    fi_update[0][i] = 0.0;
+  }
+  m_run[0] = m;
+  m_update[0] = 0.0;
+  // cost is for every pair in the upper triangle
+  for (int i = 0; i < (n_Demes-1); i++) {
+    for (int j = i+1; j < n_Demes; j++) {
+      for (int k = 0; k < n_Kpairmax; k++){
+        if (gendist_arr[i][j][k] != -1) {
+          cost[0] += pow( (gendist_arr[i][j][k] - ((fvec[i] + fvec[j])/2) *
+            exp(-m*geodist_mat[i][j])), 2);
+        }
+      }
+    }
+  }
+  // Catch and Cap Extreme Costs
+  if (cost[0] > OVERFLO_DOUBLE) {
+    cost[0] = OVERFLO_DOUBLE;
+  }
+
 
 
   //-------------------------------
@@ -91,28 +116,6 @@ Rcpp::List deme_inbreeding_coef_cpp(Rcpp::List args, Rcpp::List args_functions, 
 
 
     //-------------------------------
-    // get current cost for given Fvector and M
-    //-------------------------------
-    // cost is for every pair in the upper triangle
-    for (int i = 0; i < (n_Demes-1); i++) {
-      for (int j = i+1; j < n_Demes; j++) {
-        for (int k = 0; k < n_Kpairmax; k++){
-          if (gendist_arr[i][j][k] != -1) {
-            cost[step] += pow( (gendist_arr[i][j][k] - ((fvec[i] + fvec[j])/2) *
-              exp(-m*geodist_mat[i][j])), 2);
-          }
-        }
-      }
-    }
-
-    //-------------------------------
-    // Catch and Cap Extreme Costs
-    //-------------------------------
-    if (cost[step] > OVERFLO_DOUBLE) {
-      cost[step] = OVERFLO_DOUBLE;
-    }
-
-    //-------------------------------
     // F gradient
     // N.B. needs to be complete row (not just triangle) in order for all
     // sample i's to be accounted for in the gradient (fi + fj where j can be i)
@@ -122,16 +125,13 @@ Rcpp::List deme_inbreeding_coef_cpp(Rcpp::List args, Rcpp::List args_functions, 
     vector<double> fgrad(n_Demes);
     fill(fgrad.begin(), fgrad.end(), 0);
 
-
     // step through partial deriv for each Fi
     for (int i = 0; i < n_Demes; i++) {
       for (int j = 0; j < n_Demes; j++) {
-        if (i != j) { // -1 to catch demes that do not contain max members in array
-          for (int k = 0; k < n_Kpairmax; k++){
-            if (gendist_arr[i][j][k] != -1) {
-              fgrad[i] += -gendist_arr[i][j][k] * exp(-geodist_mat[i][j] * m) +
-                ((fvec[i] + fvec[j])/2) * exp(-2*geodist_mat[i][j] * m);
-            }
+        for (int k = 0; k < n_Kpairmax; k++){
+          if (gendist_arr[i][j][k] != -1) { // NB -1 includes self deme comparisons i = j, as well as demes hat do not contain max members in array
+            fgrad[i] += -gendist_arr[i][j][k] * exp(-geodist_mat[i][j] * m) +
+              ((fvec[i] + fvec[j])/2) * exp(-2*geodist_mat[i][j] * m);
           }
         }
       }
@@ -143,6 +143,7 @@ Rcpp::List deme_inbreeding_coef_cpp(Rcpp::List args, Rcpp::List args_functions, 
     //-------------------------------
     // clear previous step results
     double mgrad = 0;
+
     // step through partial deriv for M
     for (int i = 0; i < n_Demes; i++) {
       for (int j = 0; j < n_Demes; j++) {
@@ -204,6 +205,26 @@ Rcpp::List deme_inbreeding_coef_cpp(Rcpp::List args, Rcpp::List args_functions, 
    m_run[step] = m;
 
 
+   //-------------------------------
+   // get updated cost for given F and M
+   //-------------------------------
+   // cost is for every pair in the upper triangle
+   for (int i = 0; i < (n_Demes-1); i++) {
+     for (int j = i+1; j < n_Demes; j++) {
+       for (int k = 0; k < n_Kpairmax; k++){
+         if (gendist_arr[i][j][k] != -1) {
+           cost[step] += pow( (gendist_arr[i][j][k] - ((fvec[i] + fvec[j])/2) *
+             exp(-m*geodist_mat[i][j])), 2);
+         }
+       }
+     }
+   }
+   // Catch and Cap Extreme Costs
+   if (cost[step] > OVERFLO_DOUBLE) {
+     cost[step] = OVERFLO_DOUBLE;
+   }
+
+
   } // end steps
 
   //-------------------------------
@@ -220,6 +241,7 @@ Rcpp::List deme_inbreeding_coef_cpp(Rcpp::List args, Rcpp::List args_functions, 
 
                             Rcpp::Named("f_grad2") = f_grad2,
                             Rcpp::Named("m_grad2") = m_grad2,
+                              Rcpp::Named("mcall") = mcall,
 
                             Rcpp::Named("cost") = cost,
                             Rcpp::Named("Final_Fis") = fvec,
