4293e9344bf1a8b0de8c8c1bed699524ff7d2e4a
Nicholas Brazeau <nbrazeau@med.unc.edu>
2022-11-23 14:38:46 -0500

    fix for local slurm

--- validation/_future_polySimIBD.R
+++ validation/_future_polySimIBD.R
@@ -34,7 +34,8 @@ locatcomb <- readRDS("mkdata/simdata/locatcombo.rds")
 # run simulations on slurm
 #...........................................................
 ret <- maestro %>% dplyr::select(c("modname", "rep"))
-plan(future.batchtools::batchtools_slurm, workers = availableCores()-1)
+plan(future.batchtools::batchtools_slurm, workers = availableCores(),
+     template = "slurm_discent.tmpl")
 ret$discdat <- furrr::future_pmap(maestro[,c("pos", "N", "m", "rho", "mean_coi", "tlim", "migr_mat")],
                                   swfsim_2_discdat_wrapper,
                                   dwnsmplnum = 5,
