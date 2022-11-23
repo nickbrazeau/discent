6ff3536028b6e88e8e456cc5c92fe62e66faf1f8
Nicholas Brazeau <nbrazeau@med.unc.edu>
2022-11-23 02:27:03 -0500

    OOM fix

--- validation/run.sh
+++ validation/run.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1                    # Run all processes on a single node
 #SBATCH --ntasks=1                  # Number of MPI tasks (i.e. processes)
 #SBATCH --cpus-per-task=128
-#SBATCH --mem-per-cpu=600mb          # Memory (i.e. RAM) per processor
+#SBATCH --mem-per-cpu=256G          # Memory (i.e. RAM) per processor
 #SBATCH --time=36:00:00              # Wall time limit (days-hrs:min:sec)
 #SBATCH --output=polysim_%j.log     # Path to the standard output and error files
 
