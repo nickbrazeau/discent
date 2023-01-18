e764ef1d39e1523724b2c09e22cdf680125bf01e
Nicholas Brazeau <nbrazeau@med.unc.edu>
2023-01-17 21:59:02 -0500

    update tests

--- tests/testthat/test-Mbound.R
+++ tests/testthat/test-Mbound.R
@@ -132,26 +132,26 @@ test_that("M is properly bounded", {
   # set seed
   set.seed(48)
   # sim data
-  dat <- sim_IBDIBD(demesize = c(5,5), distmat = matrix(c(0,1e4,1e4,0), nrow = 2),
-                    rate = 1e-3, Ft = 0.5)
+  dat <- sim_IBDIBD(demesize = c(12,12), distmat = matrix(c(0,1e3,1e3,0), nrow = 2),
+                    rate = 1e-2, Ft = 0.3)
   # start params
   our_start_params <- rep(0.2, 2)
   names(our_start_params) <- 1:2
-  our_start_params <- c(our_start_params, "m" = 1e-2)
+  our_start_params <- c(our_start_params, "m" = 1e-5)
   # run model
   inputdisc <- dat %>%
     dplyr::filter(deme1 != deme2)
   mod <- deme_inbreeding_spcoef(discdat = inputdisc,
                                 start_params = our_start_params,
-                                f_learningrate = 1e-5,
-                                m_learningrate = 1e-8,
-                                m_lowerbound = 1e-3,
+                                f_learningrate = 1e-10,
+                                m_learningrate = 1e-15,
+                                m_lowerbound = 1e-8,
                                 m_upperbound = 1,
                                 momentum = 0.9,
                                 steps = 1e2,
                                 report_progress = TRUE,
                                 return_verbose = TRUE)
-  testthat::expect_gte(min(mod$m_run), 1e-3)
+  testthat::expect_gte(min(mod$m_run), 1e-8)
   testthat::expect_lte(max(mod$m_run), 1)
   testthat::expect_gt(length(unique(mod$m_run)), 1)
 })
