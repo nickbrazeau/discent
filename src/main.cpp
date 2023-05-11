f1933837ea21dec6f92392a0a26f3243dc90700f
Nicholas Brazeau <nbrazeau@med.unc.edu>
2023-05-11 13:03:41 -0400

    fix negative sign in m gradient

--- src/main.cpp
+++ src/main.cpp
@@ -135,11 +135,8 @@ Rcpp::List deme_inbreeding_coef_cpp(Rcpp::List args, Rcpp::List args_functions, 
         if (i != j) { // redundant w/ R catch and -1 below, but extra protective
           for (int k = 0; k < n_Kpairmax; k++){
             if (gendist_arr[i][j][k] != -1) {
-              mgrad += -2 * pow(1/m, 2) * gendist_arr[i][j][k] * geodist_mat[i][j] * ((fvec[i] + fvec[j])/2) *
-                exp(-geodist_mat[i][j] / m) -
-                2 * geodist_mat[i][j] * pow(1/m, 2) *
-                ((pow(fvec[i], 2) + 2 * fvec[i] * fvec[j] + pow(fvec[j], 2))/4) *
-                exp(-2 * geodist_mat[i][j] / m);
+              mgrad += -2 * pow(1/m, 2) * gendist_arr[i][j][k] * geodist_mat[i][j] * ((fvec[i] + fvec[j])/2) * exp(-geodist_mat[i][j] / m) + 
+              2 * geodist_mat[i][j] * pow(1/m, 2) * ((pow(fvec[i], 2) + 2 * fvec[i] * fvec[j] + pow(fvec[j], 2))/4) * exp(-2 * geodist_mat[i][j] / m);
             }
           }
         }
